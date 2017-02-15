e85b869c470e7e400dc2e927bd1aaba1a00454f9
文优 <thisfin@gmail.com>
2017-02-15 17:23:57 +0800

    update

---
+++ SudokuTool/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SudokuTool
+//
+//  Created by wenyou on 2017/2/15.
+//  Copyright © 2017年 wenyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //    let collectionView = UICollectionView.init(frame: .zero, collectionViewLayout: nil)
+    let datas: [[Int]] = Array(repeating: Array(repeating: 0, count: 9), count: 9)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    func getRange(index: Int) -> [Int] {
+        let start = index / 3
+        var array: [Int] = Array()
+        stride(from: start, to: start + 3, by: 1).forEach { (i) in
+            array.append(i)
+        }
+        return array
+    }
+}
